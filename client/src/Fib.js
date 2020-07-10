f6b8c3cf523661ca4fd1dd3e472a5afce85e4801
Clay Morris <clay@phiguru.net>
2020-07-10 15:07:35 -0400

    changing to course files

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -3,66 +3,72 @@ import axios from 'axios';
 
 class Fib extends Component {
   state = {
-      seenIndexes: [],
-      values: {},
-      index: ''
+    seenIndexes: [],
+    values: {},
+    index: ''
   };
-  componentDidMount() {
-      this.fetchValues();
-      this.fetchIndexes();
-  }
-  async fetchValues() {
-      const values = await axios.get('/api/values/current');
-      this.setState({values: values.data});
-  }
-  async fetchIndexes() {
-      const seenIndexes = await axios.get('/api/values/all');
-      this.setState({
-          seenIndexes: seenIndexes.data
-  });
-}
 
-  handleSubmit = async (event) => {
-      event.preventDefault();
-  
-      await axios.post('/api/values', {
-          index: this.state.index
-      });
-      this.setState({ index: '' });
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
+
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this.setState({ index: '' });
   };
 
   renderSeenIndexes() {
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
-  rednerValues() {
-      const entries  = [];
 
-      for (let key in this.state.values) {
-          entries.push(
-              <div key={key}>
-                  For index {key} I calculated {this.state.values[key]}
-              </div>
-          )
-      }
-  };
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  }
 
   render() {
     return (
       <div>
-          <form onSubmit={this.handleSubmit}>
-            <label> Enter your index:</label>
-              <input 
-                value={this.state.index}
-                onChange={event => this.setState({ index: event.target.value })}
-              />
-              <button>yes please</button>
-          </form>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
 
-          <h3>Tiddies I have seen:</h3>
-            {this.renderSeenIndexes()}
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
 
-          <h3>Calculated Tiddies:</h3>
-          {this.renderValues()}
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
       </div>
     );
   }
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => {
-    return (
-        <div>
-            I love tiddies!
-            <link to="/">Go back home</link>
-        </div>
-    );
+  return (
+    <div>
+      Im some other page
+      <Link to="/">Go back to home page!</Link>
+    </div>
+  );
 };
